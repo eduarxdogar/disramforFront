7c29e8dac873e3a96f2d3eb811beedee99ecb857
eduarxdogar <cristiangarzon1224@hotmail.com>
2025-07-25 14:47:57 -0500

    actualizacion en la nav bar  botones y logos se añaden img para el redme

--- src/app/shared/navbar/navbar.component.ts
+++ src/app/shared/navbar/navbar.component.ts
@@ -1,14 +1,22 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
 import { RouterModule } from '@angular/router';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatButtonModule }  from '@angular/material/button';
+import { MatIcon } from '@angular/material/icon';
+import { MatMenuModule } from '@angular/material/menu';
 
 @Component({
   selector: 'app-navbar',
-  imports: [RouterModule, MatToolbarModule, MatButtonModule],
+  imports: [RouterModule, MatToolbarModule, MatButtonModule, MatIcon,MatMenuModule,CommonModule],
   templateUrl: './navbar.component.html',
   styleUrl: './navbar.component.css'
 })
 export class NavbarComponent {
+  mobileMenuOpen = false;
 
+  toggleMobileMenu() {
+    this.mobileMenuOpen = !this.mobileMenuOpen;
+  }
+  
 }
